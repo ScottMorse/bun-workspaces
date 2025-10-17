4d366842ce743daa50248465543b37948997c26d
ScottMorse <scottmorsedev@gmail.com>
2025-10-16 21:31:00 -0600

    Format

--- packages/doc-website/src/content/cli/cliProjectCommandOptions.tsx
+++ packages/doc-website/src/content/cli/cliProjectCommandOptions.tsx
@@ -15,13 +15,13 @@ export type CliProjectCommandContent = Omit<
 
 const defineOptionContent = (
   optionName: ProjectCommandName,
-  factory: (optionConfig: ProjectCommandConfig) => CliOptionContent
+  factory: (optionConfig: ProjectCommandConfig) => CliOptionContent,
 ): CliProjectCommandContent => {
   const config = getProjectCommandConfig(optionName);
   const content = factory(config);
 
   const exampleLines = content.examples.filter(
-    (example) => example.trim() && !example.match(/^\s*#/)
+    (example) => example.trim() && !example.match(/^\s*#/),
   );
 
   const getMainFlag = (flag: string) => {
@@ -33,7 +33,7 @@ const defineOptionContent = (
       !exampleLines.find((line) => line.includes(getMainFlag(option.flags)))
     ) {
       throw new Error(
-        `Expected an example to include ${getMainFlag(option.flags)}`
+        `Expected an example to include ${getMainFlag(option.flags)}`,
       );
     }
   }
@@ -42,7 +42,7 @@ const defineOptionContent = (
     !exampleLines.find((line) => {
       // line that uses no flags
       return Object.values(config.options).every(
-        (option) => !line.includes(getMainFlag(option.flags))
+        (option) => !line.includes(getMainFlag(option.flags)),
       );
     })
   ) {
