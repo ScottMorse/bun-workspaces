4601a6a13d16db6ed88cb23277e672c6e32b20a0
ScottMorse <scottmorsedev@gmail.com>
2025-11-15 15:32:34 -0700

    Fix test project path

--- packages/bun-workspaces/tests/workspaceConfig.test.ts
+++ packages/bun-workspaces/tests/workspaceConfig.test.ts
@@ -315,7 +315,7 @@ describe("Test workspace config", () => {
     });
 
     expect(warnSpy).toHaveBeenCalledWith(
-      `Found config for workspace at path "/home/scott/dev/projects/bun-workspaces/packages/bun-workspaces/tests/testProjects/workspaceConfig/deprecatedConfigMix/libraries/library-a" in both package.json and bw.workspace.json. The config in bw.workspace.json will be used.`,
+      `Found config for workspace at path "${getProjectRoot("workspaceConfigDeprecatedConfigMix")}/libraries/library-a" in both package.json and bw.workspace.json. The config in bw.workspace.json will be used.`,
     );
 
     expect(project.workspaces).toEqual([
