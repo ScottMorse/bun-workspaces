6959a6587a58feb782908bd3af509ea36d89a1d4
ScottMorse <scottmorsedev@gmail.com>
2025-11-28 15:39:25 -0700

    Run format

--- packages/doc-website/src/content/cli/cliProjectCommandOptions.tsx
+++ packages/doc-website/src/content/cli/cliProjectCommandOptions.tsx
@@ -15,13 +15,13 @@ export type CliProjectCommandContent = Omit<
 
 const defineOptionContent = (
   optionName: CliProjectCommandName,
-  factory: (optionConfig: CliProjectCommandConfig) => CliOptionContent
+  factory: (optionConfig: CliProjectCommandConfig) => CliOptionContent,
 ): CliProjectCommandContent => {
   const config = getProjectCommandConfig(optionName);
   const content = factory(config);
 
   const exampleLines = content.examples.filter(
-    (example) => example.trim() && !example.match(/^\s*#/)
+    (example) => example.trim() && !example.match(/^\s*#/),
   );
 
   const getMainFlag = (flag: string) => {
@@ -33,7 +33,7 @@ const defineOptionContent = (
       !exampleLines.find((line) => line.includes(getMainFlag(option.flags)))
     ) {
       throw new Error(
-        `Expected an example to include ${getMainFlag(option.flags)}`
+        `Expected an example to include ${getMainFlag(option.flags)}`,
       );
     }
   }
@@ -42,7 +42,7 @@ const defineOptionContent = (
     !exampleLines.find((line) => {
       // line that uses no flags
       return Object.values(config.options).every(
-        (option) => !line.includes(getMainFlag(option.flags))
+        (option) => !line.includes(getMainFlag(option.flags)),
       );
     })
   ) {
@@ -163,7 +163,7 @@ const CLI_PROJECT_COMMAND_OPTIONS_CONTENT = {
 } as const satisfies Record<CliProjectCommandName, CliProjectCommandContent>;
 
 export const getCliProjectCommandContent = (
-  commandName: CliProjectCommandName
+  commandName: CliProjectCommandName,
 ) => CLI_PROJECT_COMMAND_OPTIONS_CONTENT[commandName];
 
 export const getCliProjectCommandsContent = () =>
