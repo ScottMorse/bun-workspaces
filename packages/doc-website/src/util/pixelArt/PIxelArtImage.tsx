3c61d348900a677ad13a7f10176c8529d3ee1ce4
ScottMorse <scottmorsedev@gmail.com>
2025-12-20 15:04:00 -0700

    Update spritesheets

---
+++ packages/doc-website/src/util/pixelArt/PIxelArtImage.tsx
@@ -0,0 +1,7 @@
+export interface PixelArtImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  path: string;
+}
+
+export const PixelArtImage = ({ path, ...props }: PixelArtImageProps) => {
+  return <img src={path} className="pixel-art-image" {...props} />;
+};
