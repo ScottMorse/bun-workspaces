f23b29070fd16ccc6b622fb5025b46d70f7fbc34
ScottMorse <scottmorsedev@gmail.com>
2026-01-20 13:21:59 -0700

    Attempt fix windows test

--- packages/bun-workspaces/setupTests.ts
+++ packages/bun-workspaces/setupTests.ts
@@ -24,7 +24,6 @@ for (const file of new Glob("**/*/package.json").scanSync({
             command: "bun install",
             workingDirectory: path.dirname(file),
           },
-          shell: "bun",
         }).exit;
       } catch (error) {
         console.error(`Error installing dependencies for ${file}:`, error);
